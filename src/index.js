82673318326eda5fb7a0fc0dd82b1f151c4fef16
Evgeni-Borovik <anyuser1980@gmail.com>
2019-03-17 17:12:07 +0300

    feature: add love triangles calculator

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count_lovers = 0;
+  preferences.forEach(function (k, n) {
+    var first_lover = n + 1;
+    var second_lover = k;
+    var third_lover = preferences[second_lover - 1];
+    if (first_lover === preferences[third_lover - 1]) {
+      count_lovers++;
+    };
+  })
+  var triangles = Math.floor(count_lovers / 3);
+  return triangles;
 };
